6d4468665becaa78bbdb93d0a1a5c88859bb5ab5
woodbrittany <woodbrittany3@gmail.com>
2022-01-14 11:24:20 -0600

    Task Tracker React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState
+  (true)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Monday',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Tuesday',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Friday',
+        reminder: false,
+    },
+])
+
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() *
+  10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
 }
 
-export default App;
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==
+  id))
+}
+
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+     task.id === id ? { ...task, reminder: 
+    !task.reminder } : task
+    )
+  )
+}
+
+  return (
+    <div className='container'>
+      <Header 
+      onAdd={() => setShowAddTask
+      (!showAddTask)}
+      showAdd= {showAddTask} 
+      />
+      {showAddTask && <AddTask onAdd={addTask}
+      />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} />
+  ) : (
+    'NO Tasks TO Show'
+  )}
+    </div>
+  )
+}
+
+export default App
